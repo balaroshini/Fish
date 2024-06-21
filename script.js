c6f9eb695e7e9ebe845cf7663e8e75b4034b1739
Balaroshini <balaroshne2k2@gmail.com>
2024-06-21 00:34:43 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let fill = 90;
+let intervalid = null;
+const fishbowl = document.getElementById('fishbowl');
+const fish = document.getElementById('fish')
+const tap = document.getElementById('tap')
+
+const emptyingFn = () => setInterval (() => {
+    fill = fill -1;
+    fishbowl.style = '__filling: ${fill}';
+    if (fill <)
+}
+
